a1f086adb2a48dbe5a2131a6d28adf200b3ab141
0x0025 <116722706+0x002500@users.noreply.github.com>
2023-07-02 10:42:30 +0000

    [fix](init.go) fix the description

--- cmd/init.go
+++ cmd/init.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "install the Hikari-CMS",
+	Long:  `use this command to install the Hikari-CMS in your server`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
