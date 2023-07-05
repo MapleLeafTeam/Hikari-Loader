81cb864ed71ed9ebe51eab8a1c32ccb0ecd1949d
0x0025 <116722706+0x002500@users.noreply.github.com>
2023-07-05 03:09:28 +0000

    [add]add a script for install

---
+++ script/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git clone https://github.com/MapleLeafTeam/Hikari-Core.git
+echo "下载core完成"
+pip install poetry
+echo "安装poetry完成"
+cd Hikari-Core && poetry install
+echo "安装依赖完成"
+poetry run uvicorn main:app --host 0.0.0.0 --port 8080
+echo "它在运行了！"
