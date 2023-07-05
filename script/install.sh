a56bbab30f48d0c8fa17b11375f1ed72e5ef628a
0x0025 <116722706+0x002500@users.noreply.github.com>
2023-07-05 03:21:33 +0000

    [fix](install.sh) run the init.py before start

--- script/install.sh
+++ script/install.sh
@@ -5,5 +5,7 @@ pip install poetry
 echo "安装poetry完成"
 cd Hikari-Core && poetry install
 echo "安装依赖完成"
+python init.py
+echo "初始化完成"
 poetry run uvicorn main:app --host 0.0.0.0 --port 8080
 echo "它在运行了！"
